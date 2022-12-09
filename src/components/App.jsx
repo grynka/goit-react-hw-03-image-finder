1c4626c5fb3a05a36f6222b1a14034d3837637a5
grynka <ihor.kozhemyakin@gmail.com>
2022-12-09 21:25:47 +0200

    api

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { render } from "@testing-library/react";
+import { Component } from "react";
+
+export default class App extends Component {
+  state = {
+    images: {}
+  };
+
+
+  componentDidMount() {
+    fetch(
+      'https://pixabay.com/api/?q=cat&page=1&key=30502346-d120979d6222d217ab4c63b0e'
+    )
+      .then(res => res.json())
+      .then(console.log);
+}
+
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101'
+        }}
+      >
+       {this.state.images && <div>картинки...</div>}
+      </div>
+    );
+  };
+}
