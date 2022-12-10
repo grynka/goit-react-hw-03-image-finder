68691e13756f90a2521b48a23b71dceb7e9f128b
grynka <ihor.kozhemyakin@gmail.com>
2022-12-11 00:01:07 +0200

    Update ImageGallery.js

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -6,34 +6,44 @@ import Button from "components/Button/Button";
 
 export default class ImageGallery extends Component {
   state = {
-    images: null,
+    images: '',
     loading: false,
     page: null,
+    error: ""
   };
 
   loadMore = () => {
-    console.log(this.state.page)
+    console.log(this.state.page);
     this.setState(prevState => ({
-      page: prevState.page + 1
-    }))
-  }
+      page: prevState.page + 1,
+    }));
+  };
 
   componentDidUpdate(prevProps, prevState) {
-    if (prevProps.searchImages !== this.props.searchImages || prevState.page !== this.state.page) {
-      const URL = 'https://pixabay.com/api/';
-      const key = '30502346-d120979d6222d217ab4c63b0e';
-
-      this.setState({ loading: true, page: prevState.page + 1 });
-      console.log(this.state)
-
-      fetch(
-        `${URL}?key=${key}&q=${this.props.searchImages}&image_type=photo&orientation=horizontal&per_page=12&page=${this.state.page + 1}`
-      )
-        .then(res => res.json())
-        .then(hits => this.setState({ images: [...this.state.images],  hits }))
-        .finally(this.setState({ loading: false }));
+    if (
+      prevProps.searchImages !== this.props.searchImages ||
+      prevState.page !== this.state.page
+    ) {
+      this.loadImages();
     }
   }
+
+  loadImages = () => {
+    const URL = 'https://pixabay.com/api/';
+    const key = '30502346-d120979d6222d217ab4c63b0e';
+
+    this.setState({ loading: true });
+     console.log(this.state.images);
+
+    fetch(
+      `${URL}?key=${key}&q=${this.props.searchImages}&image_type=photo&orientation=horizontal&per_page=12&page=${this.state.page}`
+    )
+      .then(res => res.json())
+      .then(hits => this.setState((prevState) => ({ images: [prevState.images, ...hits.hits], error: '' })))
+      .catch(error => this.setState({error: 'Error while loading data. Try again later.'}))
+      .finally(this.setState({ loading: false }));
+  };
+
   render() {
     const { images, loading, page } = this.state;
     return (
@@ -41,9 +51,11 @@ export default class ImageGallery extends Component {
         <Gallery className="gallery">
           {loading && <Loader />}
           {images &&
-            images.map(image => <ImageGalleryItem key={image.id}> {image}</ImageGalleryItem>)}
+            images.map(image => (
+              <ImageGalleryItem key={image.id}> {image}</ImageGalleryItem>
+            ))}
         </Gallery>
-        {page && <button onClick={this.loadMore}>load more...</button>}
+        {page && <button onClick={this.loadMore} />}
       </>
     );
   }
